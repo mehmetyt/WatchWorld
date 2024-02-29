950427298b5cec85c2ff45451c1a9e07b9ec840e
mehme <mehmetyigit.mail@gmail.com>
2024-02-29 12:27:16 +0300

    Seed users

---
+++ src/ApplicationCore/Constants/AuthorizationConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Constants
+{
+	public class AuthorizationConstants
+	{
+		public const string DEFAULT_DEMO_USER = "demouser@example.com";
+		public const string DEFAULT_ADMİN_USER = "admin@example.com";
+		public const string DEFAULT_PASSWORD = "P@sword1";
+
+		public static class Roles
+		{
+			public const string ADMINISTRATOR = "Admin";
+		}
+	}
+}
