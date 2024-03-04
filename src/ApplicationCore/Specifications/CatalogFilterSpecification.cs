bf5bc0e99c2ed11aeb429d16e4d44995feee11c1
mehme <mehmetyigit.mail@gmail.com>
2024-03-04 12:29:05 +0300

    Index

---
+++ src/ApplicationCore/Specifications/CatalogFilterSpecification.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Specifications
+{
+	public class CatalogFilterSpecification:Specification<Product>
+	{
+        public CatalogFilterSpecification(int? categryId, int? brandId)
+        {
+            if(categryId.HasValue)
+                Query.Where(x => x.CategoryId == categryId);
+            if(brandId.HasValue)
+                Query.Where(x => x.BrandId == brandId);
+        }
+    }
+}
